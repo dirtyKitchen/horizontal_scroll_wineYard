9414b9e3a3d1283d9e6d3c9be2bf53b6e52a0321
dirtyKitchen <mydirtykitchen@outlook.com>
2022-11-24 16:40:32 +0000

    style: add mouse effect for quote

--- src/cursor.js
+++ src/cursor.js
@@ -1,5 +1,9 @@
 const mycursor = document.querySelector('#cursor')
 const forHover = document.querySelectorAll('.forHover')
+const q1 = document.querySelector('.q1');
+const a1 = document.querySelector('.a1')
+console.log(q1)
+
 
 for (let i = 0; i < forHover.length; i++) {
     forHover[i].addEventListener('mouseover', _ => {
@@ -14,3 +18,11 @@ for (let i = 0; i < forHover.length; i++) {
         mycursor.style.border = "2px solid black"
     })
 }
+
+q1.addEventListener('mouseenter', _ => {
+    q1.style.transform = "scale(1.5, 1.5)"
+})
+
+q1.addEventListener('mouseleave', _ => {
+    q1.style.transform = "scale(1.0, 1.0)"
+})
